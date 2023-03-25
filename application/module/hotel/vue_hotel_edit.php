ce31ce4e683628d4bf8b6a51e758d727872e5c4a
pingouin <ouanyou21@gmail.com>
2023-03-25 18:38:53 +0100

    traitement de la vue edit hotel

--- application/module/hotel/vue_hotel_edit.php
+++ application/module/hotel/vue_hotel_edit.php
@@ -23,17 +23,17 @@
         </div>
         <div class='form-group'>
             <label for='hot_longitude'>Longitude</label>
-            <input id='hot_longitude' name='hot_longitude' type='float' size='50' value='<?= mhe($hot_longitude) ?>' class='form-control' />
+            <input id='hot_longitude' name='hot_longitude' type='number' step='0.01' size='50' value='<?= mhe($hot_longitude) ?>' class='form-control' />
         </div>
         <div class='form-group'>
             <label for='hot_latitude'>Latitude</label>
-            <input id='hot_latitude' name='hot_latitude' type='float' size='50' value='<?= mhe($hot_latitude) ?>' class='form-control' />
+            <input id='hot_latitude' name='hot_latitude' type='number' step='0.01' size='50' value='<?= mhe($hot_latitude) ?>' class='form-control' />
         </div>
         <div class='form-group'>
             <label for='hot_hocategorie'>Hocategorie</label>
         
 <select id='hot_hocategorie' name='hot_hocategorie' class='form-control'>
-                <?= Table::HTMLoptions("select * from categorie ", "chc_id", "hoc_categorie", $hot_hocategorie) ?>
+                <?= Table::HTMLoptions("select * from hocategorie ", "hoc_id", "hoc_categorie", $hot_hocategorie) ?>
             </select>
     
         </div>
