512d9b64c66b7bf8498d077252eb409af4cd92ff
cedwics10 <brochettes40@outlook.fr>
2023-03-25 18:13:10 +0100

    Edit de status et chcategorie possible

--- application/table/Chambre.class.php
+++ application/table/Chambre.class.php
@@ -17,9 +17,21 @@ class Chambre extends Table
 		cha_typelit2, cha_description, cha_jacuzzi,
 		cha_balcon, cha_wifi, cha_minibar, cha_coffre,
 		cha_vue, chc_categorie FROM chambre, chcategorie 
-		WHERE cha_chcategorie = chc_id";
+		WHERE cha_chcategorie = chc_id
+		ORDER BY cha_id 
+		";
 
 		$result = self::$link->query($sql);
 		return $result->fetchAll();
+	}
+
+	static public function OPTIONStatut($statut)
+	{
+		return self::HTMLoptions(
+			'SELECT DISTINCT cha_statut FROM chambre',
+			'cha_statut',
+			'cha_statut',
+			$statut
+		);
 	}
 }
--- application/table/Chcategorie.class.php
+++ application/table/Chcategorie.class.php
@@ -1,10 +1,17 @@
 <?php
+
 /**
 Classe créé par le générateur.
-*/
-class Chcategorie extends Table {
-	public function __construct() {
+ */
+class Chcategorie extends Table
+{
+	public function __construct()
+	{
 		parent::__construct("chcategorie", "chc_id");
 	}
+
+	static public function OPTIONchcategories($id)
+	{
+		return self::HTMLoptions('SELECT chc_id, chc_categorie FROM chcategorie', 'chc_id', 'chc_categorie', $id);
+	}
 }
-?>
