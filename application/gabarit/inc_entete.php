62dc78f47c5d7be8ad211f3d3782868f14a9f829
cedrvn10 <brochettes40@outlook.fr>
2023-03-25 12:47:40 +0100

    Logo de vivehotel remplacé

--- application/gabarit/inc_entete.php
+++ application/gabarit/inc_entete.php
@@ -1,8 +1,8 @@
 <div class="entete">
 	<div class="row">
-		<div class="col"><a href="index.php"><img src="_images/logo.gif" class="logo" ></a></div>
+		<div class="col"><a href="index.php"><img src="_images/logo.png" class="logo"></a></div>
 		<div class="col">
-			<h1 class="display-4"><?=SITE_NOM?></h1>
+			<h1 class="display-4"><?= SITE_NOM ?></h1>
 			<p class="lead">site multi users responsive avec Bootstrap</p>
 		</div>
 	</div>
