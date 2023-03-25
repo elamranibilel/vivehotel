488037b9a9e07dd45bd6e0befb79e19e17bbf696
cedwics10 <brochettes40@outlook.fr>
2023-03-25 15:21:41 +0100

    Menu modifié, mot de passe cachés pour l'administrateur et nouvelles annexes

--- application/module/client/vue_client_index.php
+++ application/module/client/vue_client_index.php
@@ -1,31 +1,29 @@
     <h2>client</h2>
-    <p><a class="btn btn-primary" href="<?=hlien("client","edit","id",0)?>">Nouveau client</a></p>
-	<table class="table table-striped table-bordered table-hover">
-		<thead>
-			<tr>
-				
-			<th>Id</th>
-			<th>Nom</th>
-			<th>Identifiant</th>
-			<th>Mdp</th>
-			<th>Email</th>
-				<th>modifier</th>
-				<th>supprimer</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php
-		foreach ( $data as $row) { ?>
-		<tr>
-			
-			<td><?=mhe($row['cli_id'])?></td>
-			<td><?=mhe($row['cli_nom'])?></td>
-			<td><?=mhe($row['cli_identifiant'])?></td>
-			<td><?=mhe($row['cli_mdp'])?></td>
-			<td><?=mhe($row['cli_email'])?></td>
-			<td><a class="btn btn-warning" href="<?=hlien("client","edit","id",$row["cli_id"])?>">Modifier</a></td>
-			<td><a class="btn btn-danger" href="<?=hlien("client","delete","id",$row["cli_id"])?>">Supprimer</a></td>
-		</tr>
-		<?php } ?>
-		</tbody>
-	</table>
+    <p><a class="btn btn-primary" href="<?= hlien("client", "edit", "id", 0) ?>">Nouveau client</a></p>
+    <table class="table table-striped table-bordered table-hover">
+    	<thead>
+    		<tr>
+
+    			<th>Id</th>
+    			<th>Nom</th>
+    			<th>Identifiant</th>
+    			<th>Email</th>
+    			<th>modifier</th>
+    			<th>supprimer</th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<?php
+			foreach ($data as $row) { ?>
+    			<tr>
+
+    				<td><?= mhe($row['cli_id']) ?></td>
+    				<td><?= mhe($row['cli_nom']) ?></td>
+    				<td><?= mhe($row['cli_identifiant']) ?></td>
+    				<td><?= mhe($row['cli_email']) ?></td>
+    				<td><a class="btn btn-warning" href="<?= hlien("client", "edit", "id", $row["cli_id"]) ?>">Modifier</a></td>
+    				<td><a class="btn btn-danger" href="<?= hlien("client", "delete", "id", $row["cli_id"]) ?>">Supprimer</a></td>
+    			</tr>
+    		<?php } ?>
+    	</tbody>
+    </table>
--- application/module/personnel/vue_personnel_index.php
+++ application/module/personnel/vue_personnel_index.php
@@ -1,35 +1,33 @@
     <h2>personnel</h2>
-    <p><a class="btn btn-primary" href="<?=hlien("personnel","edit","id",0)?>">Nouveau personnel</a></p>
-	<table class="table table-striped table-bordered table-hover">
-		<thead>
-			<tr>
-				
-			<th>Id</th>
-			<th>Nom</th>
-			<th>Identifiant</th>
-			<th>Mdp</th>
-			<th>Email</th>
-			<th>Role</th>
-			<th>Hotel</th>
-				<th>modifier</th>
-				<th>supprimer</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php
-		foreach ( $data as $row) { ?>
-		<tr>
-			
-			<td><?=mhe($row['per_id'])?></td>
-			<td><?=mhe($row['per_nom'])?></td>
-			<td><?=mhe($row['per_identifiant'])?></td>
-			<td><?=mhe($row['per_mdp'])?></td>
-			<td><?=mhe($row['per_email'])?></td>
-			<td><?=mhe($row['per_role'])?></td>
-			<td><?=mhe($row['per_hotel'])?></td>
-			<td><a class="btn btn-warning" href="<?=hlien("personnel","edit","id",$row["per_id"])?>">Modifier</a></td>
-			<td><a class="btn btn-danger" href="<?=hlien("personnel","delete","id",$row["per_id"])?>">Supprimer</a></td>
-		</tr>
-		<?php } ?>
-		</tbody>
-	</table>
+    <p><a class="btn btn-primary" href="<?= hlien("personnel", "edit", "id", 0) ?>">Nouveau personnel</a></p>
+    <table class="table table-striped table-bordered table-hover">
+    	<thead>
+    		<tr>
+
+    			<th>Id</th>
+    			<th>Nom</th>
+    			<th>Identifiant</th>
+    			<th>Email</th>
+    			<th>Role</th>
+    			<th>Hotel</th>
+    			<th>modifier</th>
+    			<th>supprimer</th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<?php
+			foreach ($data as $row) { ?>
+    			<tr>
+
+    				<td><?= mhe($row['per_id']) ?></td>
+    				<td><?= mhe($row['per_nom']) ?></td>
+    				<td><?= mhe($row['per_identifiant']) ?></td>
+    				<td><?= mhe($row['per_email']) ?></td>
+    				<td><?= mhe($row['per_role']) ?></td>
+    				<td><?= mhe($row['per_hotel']) ?></td>
+    				<td><a class="btn btn-warning" href="<?= hlien("personnel", "edit", "id", $row["per_id"]) ?>">Modifier</a></td>
+    				<td><a class="btn btn-danger" href="<?= hlien("personnel", "delete", "id", $row["per_id"]) ?>">Supprimer</a></td>
+    			</tr>
+    		<?php } ?>
+    	</tbody>
+    </table>
