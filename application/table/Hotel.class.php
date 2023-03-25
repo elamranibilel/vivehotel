043b0cae5822d037e7a1f993a12d79fb8b0cb958
pingouin <ouanyou21@gmail.com>
2023-03-25 15:47:24 +0100

    requete de la table hotel

--- application/table/Hotel.class.php
+++ application/table/Hotel.class.php
@@ -6,5 +6,13 @@ class Hotel extends Table {
 	public function __construct() {
 		parent::__construct("hotel", "hot_id");
 	}
+	
+// creation d'une table hotel categore
+public function selectAllcategorie(): array
+{
+	$sql="select * from hotel, hocategorie where hot_id=hoc_categorie order by hot_id;";
+	        $result=self::$link->query($sql);
+        return $result->fetchAll();
+}
 }
 ?>
