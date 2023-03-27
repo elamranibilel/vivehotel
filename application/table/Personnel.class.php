8ebcbb81dd0ed1a0f467868d429ac579401a85cf
cedwics10 <brochettes40@outlook.fr>
2023-03-27 11:41:15 +0200

    CRUD du personnel - début

--- application/table/Personnel.class.php
+++ application/table/Personnel.class.php
@@ -1,10 +1,29 @@
 <?php
+
 /**
 Classe créé par le générateur.
-*/
-class Personnel extends Table {
-	public function __construct() {
+ */
+class Personnel extends Table
+{
+	public function __construct()
+	{
 		parent::__construct("personnel", "per_id");
 	}
+
+	public function selectAll(): array
+	{
+		$sql = "SELECT
+		per_id,	
+		per_nom,
+		per_identifiant,	
+		per_mdp,
+		per_email,	
+		per_role,	
+		hot_nom
+		FROM personnel, hotel
+		WHERE per_hotel = hot_id
+		ORDER BY per_id";
+		$stmt = self::$link->query($sql);
+		return $stmt->fetchAll();
+	}
 }
-?>
