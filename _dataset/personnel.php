4d97eabc193a166f67f5131b4b7729f08860a204
cedwics10 <brochettes40@outlook.fr>
2023-03-29 14:02:25 +0200

    Suppression des personnels

--- _dataset/personnel.php
+++ _dataset/personnel.php
@@ -3,7 +3,7 @@
 //génération de personnels  
 const NOMBRE_PERSONNEL = 20;
 $nombre_hotel = 50;
-$nombre_teletconseiller = 10;
+const NOMBRE_TELEC = 10;
 $role = [
     "teleconseiller",
     "gestionnaire"
@@ -19,7 +19,7 @@ for ($i = 1; $i <= $nombre_hotel; $i++) {
     $tab[] = "(null,'$per_nom','$per_identifiant','$per_mdp','$per_email','$per_role','$per_hotel')";
 }
 
-for ($i = 1; $i <= $nombre_teletconseiller; $i++) {
+for ($i = 1; $i <= NOMBRE_TELEC; $i++) {
 
     $per_nom = "teleconseiller $i";
     $per_identifiant = "teleconseiller$i";
@@ -29,15 +29,6 @@ for ($i = 1; $i <= $nombre_teletconseiller; $i++) {
     $per_hotel = 'null';
     $tab[] = "(null,'$per_nom','$per_identifiant','$per_mdp','$per_email','$per_role',$per_hotel)";
 }
-for ($i = 1; $i <= NOMBRE_PERSONNEL; $i++) {
 
-    $per_nom = "personnel $i";
-    $per_identifiant = "personnel$i";
-    $per_mdp = password_hash($per_identifiant, PASSWORD_DEFAULT);
-    $per_email = "p$i@personnel.fr";
-    $per_role = "personnel";
-    $per_hotel = $i;
-    $tab[] = "(null,'$per_nom','$per_identifiant','$per_mdp','$per_email','$per_role','$per_hotel')";
-}
 $sql = "insert into personnel values " . implode(",", $tab);
 mysqli_query($link, $sql);
