6bba3da2a7043633e1aabbcb2bef3005f3fb6e5a
cedwics10 <brochettes40@outlook.fr>
2023-03-30 11:46:03 +0200

    Modification hotel index

--- application/module/hotel/vue_hotel_index.php
+++ application/module/hotel/vue_hotel_index.php
@@ -11,7 +11,7 @@
     			<th>Departement</th>
     			<th>Description</th>
     			<th>Catégorie</th>
-				<th>services</th>
+    			<th>services</th>
     			<th>modifier</th>
     			<th>Supprimer</th>
     		</tr>
@@ -28,13 +28,9 @@
     				<td><?= mhe($row['hot_departement']) ?></td>
     				<td><?= mhe($row['hot_description']) ?></td>
     				<td><?= mhe($row['hoc_categorie']) ?></td>
-					
-					<td><a class="btn btn-warning" href="<?= hlien("hotel", "services", "id", $row["hot_id"]) ?>">services</a></td>
-    				<td><a class=
-		
 
-
-				<td><a class="btn btn-warning" href="<?= hlien("hotel", "edit", "id", $row["hot_id"]) ?>">Modifier</a></td>
+    				<td><a class="btn btn-info" href="<?= hlien("hotel", "services", "id", $row["hot_id"]) ?>">Services</a></td>
+    				<td><a class=<td><a class="btn btn-warning" href="<?= hlien("hotel", "edit", "id", $row["hot_id"]) ?>">Modifier</a></td>
     				<td><a class="btn btn-danger" href="<?= hlien("hotel", "delete", "id", $row["hot_id"]) ?>">Supprimer</a></td>
     			</tr>
     		<?php } ?>
