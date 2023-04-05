afff419ac7d67fe40c7deba09c0e58b6424e6a43
cedwics10 <brochettes40@outlook.fr>
2023-04-05 17:27:21 +0200

    Vue de tarifer modifié

--- application/module/tarifer/vue_tarifer_index.php
+++ application/module/tarifer/vue_tarifer_index.php
@@ -1,29 +1,29 @@
-    <h2>tarifer</h2>
-    <p><a class="btn btn-primary" href="<?=hlien("tarifer","edit","id",0)?>">Nouveau tarifer</a></p>
-	<table class="table table-striped table-bordered table-hover">
-		<thead>
-			<tr>
-				
-			<th>Id</th>
-			<th>Prix</th>
-			<th>Hocategorie</th>
-			<th>Chcategorie</th>
-				<th>modifier</th>
-				<th>supprimer</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php
-		foreach ( $data as $row) { ?>
+<h2>Ensemble des tarifs des chambres</h2>
+<p><a class="btn btn-primary" href="<?= hlien("tarifer", "edit", "id", 0) ?>">Nouveau tarifer</a></p>
+
+<table>
+	<thead>
 		<tr>
-			
-			<td><?=mhe($row['tar_id'])?></td>
-			<td><?=mhe($row['tar_prix'])?></td>
-			<td><?=mhe($row['tar_hocategorie'])?></td>
-			<td><?=mhe($row['tar_chcategorie'])?></td>
-			<td><a class="btn btn-warning" href="<?=hlien("tarifer","edit","id",$row["tar_id"])?>">Modifier</a></td>
-			<td><a class="btn btn-danger" href="<?=hlien("tarifer","delete","id",$row["tar_id"])?>">Supprimer</a></td>
+			<th></th> <!-- Cellule vide pour l'intersection des titres -->
+			<th colspan="3">Catégorie d'hôtel</th>
 		</tr>
-		<?php } ?>
-		</tbody>
-	</table>
+	</thead>
+	<tbody>
+		<tr>
+			<th rowspan="3">Catégorie de chambre</th>
+			<td>*</td>
+			<td>*</td>
+			<td>*</td>
+		</tr>
+		<tr>
+			<td>*</td>
+			<td>*</td>
+			<td>*</td>
+		</tr>
+		<tr>
+			<td>*</td>
+			<td>*</td>
+			<td>*</td>
+		</tr>
+	</tbody>
+</table>
