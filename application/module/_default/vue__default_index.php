4b9efff6e6ff9361af47a0f38b5f10865bfe4e6c
cedrvn10 <brochettes40@outlook.fr>
2023-04-11 07:56:50 +0200

    Texte index modifié

--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -1,3 +1,3 @@
 <h1>Vivehotel</h1>
 <ol>
-    C'est parti pour faire un site hôtelier ! </ol>
+    Vivehotel a été l'hôtel de LUXE préféré des Français en 2022. Venez y passer une ou plusieurs nuits dans l'un des hôtels de notre grande chaîne.</ol>
