ea11240327c8d90e1aeea7cc49865fcf69503033
cedwics10 <brochettes40@outlook.fr>
2023-04-12 17:27:03 +0200

    Liens du menu dans l'ordre alphabétique

--- application/gabarit/inc_menu.php
+++ application/gabarit/inc_menu.php
@@ -12,9 +12,9 @@
         <li><a class='nav-link' href='<?= hlien("chambre", "index") ?>'>Chambre</a></li>
         <li><a class='nav-link' href='<?= hlien("client", "index") ?>'>Client</a></li>
         <li><a class='nav-link' href='<?= hlien("hotel", "index") ?>'>Hotel</a></li>
-        <li><a class='nav-link' href='<?= hlien("services", "index") ?>'>Services</a></li>
         <li><a class='nav-link' href='<?= hlien("personnel", "index") ?>'>Personnel</a></li>
-        <li><a class="nav-link" href="<?=hlien("reservation","index")?>">Reservation</a></li>
+        <li><a class="nav-link" href="<?= hlien("reservation", "index") ?>">Reservation</a></li>
+        <li><a class='nav-link' href='<?= hlien("services", "index") ?>'>Services</a></li>
         <li><a class='nav-link' href='<?= hlien("tarifer", "index") ?>'>Tarif</a></li>
 
       </ul>
