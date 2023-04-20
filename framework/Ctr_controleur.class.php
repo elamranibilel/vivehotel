c7c869adc9effccadb23f40ea94ffda7a7f5c8d9
cedwics10 <brochettes40@outlook.fr>
2023-04-20 13:56:22 +0200

    Commentaires sur le contrôleur

--- framework/Ctr_controleur.class.php
+++ framework/Ctr_controleur.class.php
@@ -1,15 +1,25 @@
 <?php
 //class mère des controleurs secondaires
-class Ctr_controleur {
-	protected string $module;
+class Ctr_controleur
+{
+    protected string $module;
     protected string $action;
     protected string $gabarit;
     protected string $vue;
-	
-	public function __construct(string $module, string $action, string $gabarit="gabarit.php") {
+
+    /**
+     * __construct
+     *
+     * @param  mixed $module nom du contrôleur actionné
+     * @param  mixed $action action qui va être lancé à partir du contrôleur
+     * @param  mixed $gabarit lien de la page de gabarit qui va charger la vue
+     * @return void la classe mère n'a qu'un simple contrôleur de définition de variables
+     */
+    public function __construct(string $module, string $action, string $gabarit = "gabarit.php")
+    {
         $this->module = $module;
         $this->action = $action;
-        $this->gabarit ="../application/gabarit/$gabarit";
+        $this->gabarit = "../application/gabarit/$gabarit";
         $this->vue = "../application/module/{$module}/vue_{$module}_{$action}.php";
     }
 }
